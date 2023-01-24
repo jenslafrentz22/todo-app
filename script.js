b2e06ea302e8fadbbde4a50e4d14ab1c804c0c06
Jens Lafrentz <j.lafrentz@web.de>
2023-01-24 12:31:46 +0100

    feat: style + bug fix

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
+// ############### State Render ######################
 const state = {
   todos: [
-    { id: "learnHTML", description: "Learn HTML", done: false },
-    { id: "learnCSS", description: "Learn CSS", done: false },
-    { id: "learnJS", description: "Learn JavaScript", done: false },
+    { id: "x", description: "Learn HTML", done: false, for: "x" },
+    { id: "y", description: "Learn CSS", done: false, for: "y" },
+    { id: "z", description: "Learn JavaScript", done: false, for: "z" },
   ],
 };
-
+// Your Todos erzeugen:
 function renderTodos() {
-  const list = document.querySelector("#list"); // <ul id=“list“> auswählen
+  const list = document.querySelector("#todo-list");
   list.innerHTML = ""; // Liste vorab leeren
 
-  // li + checkbox erzeugen, an ul anhängen
+  // li + input(checkbox) + label erzeugen, an ul anhängen
   state.todos.forEach((todo) => {
-    // für jeden Eintrag (todo) im State (todos)
+    const todoLi = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const cboxLabel = document.createElement("label");
 
-    const todoLi = document.createElement("li"); // <li>Text...</li> erzeugen
-
-    const checkbox = document.createElement("input"); // li-Checkbox („todo/done“)
+    // li stylen
+    todoLi.setAttribute("class", "todo-item");
+    // checkbox stylen
     checkbox.type = "checkbox";
-    checkbox.setAttribute("id", todo.id); // checkbox mit z. B. id="learnHTML"
-    checkbox.checked = todo.done; // checkbox auf true oder false setzen
+    checkbox.setAttribute("id", todo.id);
+    checkbox.setAttribute("class", "todo-item__checkbox");
+    checkbox.checked = todo.done;
+
+    // label stylen
+    cboxLabel.setAttribute("for", todo.for);
+    cboxLabel.setAttribute("class", "todo-item__text");
+    if (todo.done) {
+      cboxLabel.setAttribute("style", "text-decoration: line-through");
+    }
+    console.log("LABEL: ", cboxLabel);
 
     // ##### state UPDATE (geänderten Zustand speichern) ##########
     checkbox.addEventListener("change", (e) => {
-      const newTodoDoneState = e.target.checked; // Checkbox geklickt: true / false
+      const newTodoDoneState = e.target.checked;
       todo.done = newTodoDoneState;
-      
-      console.log(e.target); // Ausgabe bei Checkbox-Klick (Change)
+      // console.log(e.target); // Ausgabe bei Checkbox-Klick (Change)
     });
     // ##### state UPDATE ENDE ##########
 
+    // checkbox+label mit Todo-Text an <li> hängen
     todoLi.appendChild(checkbox);
-
     const todoText = document.createTextNode(todo.description);
     todoLi.append(todoText);
 
+    // <li> an <ul> hängen
     list.appendChild(todoLi); // <ul> <li>Text...</li> … anhängen
   });
 }
 renderTodos();
+
+// Remove Done Todos
+function removeDoneTodos() {
+  state.todos.forEach((todo) => {
+    if (todo.done) {
+      console.log("Huhu");
+      // _.innerHTML = "";
+    }
+  });
+}
+deleteButton.addEventListener("click", removeDoneTodos);
