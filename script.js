9c6b0142f3265159b283c89032c683df862b2e03
Jens Lafrentz <j.lafrentz@web.de>
2023-01-26 15:47:48 +0100

    feat: localStorage .setItem + .getItem works

--- script.js
+++ script.js
@@ -1,64 +1,153 @@
-// ############### State Render ######################
+const addButton = document.querySelector("#add-button");
+const deleteButton = document.querySelector("#delete-button");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const radioContainer = document.querySelector("#radio-container");
+
+// STATE erzeugen: id: ..., todo: todoInput.value, done: false
 const state = {
-  todos: [
-    { id: "x", description: "Learn HTML", done: false, for: "x" },
-    { id: "y", description: "Learn CSS", done: false, for: "y" },
-    { id: "z", description: "Learn JavaScript", done: false, for: "z" },
-  ],
+  todos: [],
 };
-// Your Todos erzeugen:
-function renderTodos() {
-  const list = document.querySelector("#todo-list");
-  list.innerHTML = ""; // Liste vorab leeren
 
-  // li + input(checkbox) + label erzeugen, an ul anhängen
-  state.todos.forEach((todo) => {
-    const todoLi = document.createElement("li");
-    const checkbox = document.createElement("input");
-    const cboxLabel = document.createElement("label");
+// ADD TODOS
+function addTodo() {
+  const newTodoText = todoInput.value;
+  todoInput.value = "";
 
-    // li stylen
-    todoLi.setAttribute("class", "todo-item");
-    // checkbox stylen
-    checkbox.type = "checkbox";
-    checkbox.setAttribute("id", todo.id);
-    checkbox.setAttribute("class", "todo-item__checkbox");
-    checkbox.checked = todo.done;
+  // Elemente holen
+  const newTodoLi = document.createElement("li");
+  const checkBox = document.createElement("input");
+  const cboxLabel = document.createElement("label");
 
-    // label stylen
-    cboxLabel.setAttribute("for", todo.for);
-    cboxLabel.setAttribute("class", "todo-item__text");
-    if (todo.done) {
-      cboxLabel.setAttribute("style", "text-decoration: line-through");
-    }
-    console.log("LABEL: ", cboxLabel);
+  // li stylen
+  newTodoLi.setAttribute("class", "todo-item");
+  // checkbox stylen
+  checkBox.type = "checkbox";
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("class", "todo-item__checkbox");
+  // label stylen
+  cboxLabel.setAttribute("class", "todo-item__text");
 
-    // ##### state UPDATE (geänderten Zustand speichern) ##########
-    checkbox.addEventListener("change", (e) => {
-      const newTodoDoneState = e.target.checked;
-      todo.done = newTodoDoneState;
-      // console.log(e.target); // Ausgabe bei Checkbox-Klick (Change)
-    });
-    // ##### state UPDATE ENDE ##########
+  // zusammenbauen:
+  newTodoLi.appendChild(checkBox);
+  cboxLabel.innerText = newTodoText;
+  newTodoLi.appendChild(cboxLabel);
+  todoList.appendChild(newTodoLi);
 
-    // checkbox+label mit Todo-Text an <li> hängen
-    todoLi.appendChild(checkbox);
-    const todoText = document.createTextNode(todo.description);
-    todoLi.append(todoText);
-
-    // <li> an <ul> hängen
-    list.appendChild(todoLi); // <ul> <li>Text...</li> … anhängen
+  // ############# ADD in LOCAL STORAGE #######################
+  let todoID = +new Date();
+  state.todos.push({
+    id: todoID,
+    todo: `${newTodoText}`,
+    done: "false",
   });
+  const todosAsString = JSON.stringify(state.todos);
+  localStorage.setItem("todos", todosAsString); // `${todoID}`
+  // ############# LOCAL STORAGE END #######################
 }
-renderTodos();
+addButton.addEventListener("click", addTodo);
 
-// Remove Done Todos
+// STATE (aus LocalStorage) FÜLLEN
+function loadDataFromLocalStorage() {
+  if (localStorage.getItem("todos")) {
+    const todosData = JSON.parse(localStorage.getItem("todos"));
+    return todosData;
+  } else {
+    return [];
+  }
+}
+state.todos = loadDataFromLocalStorage();
+console.log("HIER: ", state.todos);
+
+// localStorage.clear();
+// CHECKED (line-through / done)
+
+if (state.todos.length > 0) {
+  for (let k = state.todos.length; k >= 0; k--) {
+    const getIt = localStorage.getItem("todos");
+    console.log("Eintrag: ", getIt);
+  }
+}
+
+function isChecked(e) {
+  if (e.target.checked) {
+    e.target.parentElement.style.textDecoration = "line-through";
+  } else {
+    e.target.parentElement.style.textDecoration = "none";
+  }
+}
+todoList.addEventListener("change", isChecked);
+
+// REMOVE DONE TODOS
 function removeDoneTodos() {
-  state.todos.forEach((todo) => {
-    if (todo.done) {
-      console.log("Huhu");
-      // _.innerHTML = "";
+  // localStorage.getItem("eintrag:");
+
+  const children = todoList.children;
+  const length = children.length - 1;
+
+  for (let i = length; i >= 0; i--) {
+    const li = children[i];
+    const checkbox = li.querySelector('input[type="checkbox"]');
+
+    if (checkbox.checked) {
+      li.remove();
     }
-  });
+  }
 }
 deleteButton.addEventListener("click", removeDoneTodos);
+
+// #####################################################
+
+// FILTER
+function filterTodos(e) {
+  switch (e.target.value) {
+    case "all":
+      showAllTodos();
+      break;
+    case "open":
+      showOpenTodos();
+      break;
+    case "done":
+      showDoneTodos();
+      break;
+  }
+}
+radioContainer.addEventListener("change", filterTodos);
+// Filter - ALL
+function showAllTodos() {
+  for (let li of todoList.children) {
+    // li.hidden;
+    li.style.visibility = "visible";
+  }
+}
+// Filter - OPEN
+function showOpenTodos() {
+  for (let li of todoList.children) {
+    const checkbox = li.querySelector('input[type="checkbox"]');
+    const isChecked = checkbox.checked;
+    if (isChecked) {
+      // li.setAttribute("hidden", "true");
+      // li.hidden = true;
+      li.style.visibility = "hidden";
+    } else {
+      li.style.visibility = "visible";
+      // li.hidden = false;
+      // li.setAttribute("hidden", "false");
+    }
+  }
+}
+// Filter - DONE
+function showDoneTodos() {
+  console.log("DONE");
+  for (let li of todoList.children) {
+    const checkbox = li.querySelector('input[type="checkbox"]');
+    const isChecked = checkbox.checked;
+    if (isChecked) {
+      // li.hidden = false;
+      li.style.visibility = "visible";
+    } else {
+      // li.hidden = true;
+      li.style.visibility = "hidden";
+    }
+  }
+}
